17dba6d8616e43dd612f8c3410b2e57a074097c8
Edson Marques <39574375+edsoncms@users.noreply.github.com>
2025-04-02 14:31:40 -0300

    Add files via upload

---
+++ Source/NET/ExternalDatabaseExec.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Data;
+using OutSystems.HubEdition.RuntimePlatform;
+using OutSystems.RuntimePublic.Db;
+
+namespace OutSystems.NssExternalDatabaseExec {
+
+	public class CssExternalDatabaseExec: IssExternalDatabaseExec {
+
+		/// <summary>
+		/// Executes Insert, Update and Delete instructions/commands
+		/// </summary>
+		/// <param name="ssCommand"></param>
+		/// <param name="ssErrorMessage"></param>
+		public void MssExecInsUpdDel(string ssDBConnectionName, string ssCommand, out string ssErrorMessage) {
+			ssErrorMessage = "";
+			// TODO: Write implementation for action
+		} // MssExecInsUpdDel
+
+		/// <summary>
+		/// Executes SQL queries and returns a JSON with the result
+		/// </summary>
+		/// <param name="ssQuery"></param>
+		/// <param name="ssResult"></param>
+		/// <param name="ssErrorMessage"></param>
+		public void MssExecQuery(string ssDBConnectionName, string ssQuery, out string ssResult, out string ssErrorMessage) {
+			ssResult = "";
+			ssErrorMessage = "";
+			// TODO: Write implementation for action
+		} // MssExecQuery
+
+	} // CssExternalDatabaseExec
+
+} // OutSystems.NssExternalDatabaseExec
+
---
+++ Source/NET/Utils.cs
@@ -0,0 +1,77 @@
+﻿using OutSystems.RuntimePublic.Db;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Text.Json;
+
+namespace OutSystems.NssExternalDatabaseExec
+{
+    internal class Utils
+    {
+
+        public static int ReadSequenceValue(string dbConnectionName, string sequenceName)
+        {
+            var databaseProvider = DatabaseAccess.ForExternalDatabase(dbConnectionName);
+            using (var transaction = databaseProvider.GetRequestTransaction())
+            {
+                using (var command = transaction.CreateCommand($"SELECT "+dbConnectionName+".{"+sequenceName+"}.NEXTVAL FROM DUAL"))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        return reader.GetInt32(0);
+                    }
+                }
+            }
+        }
+
+        public static string ExecInsUpdDel(string dbConnectionName, string sql)
+        {
+            var databaseProvider = DatabaseAccess.ForExternalDatabase(dbConnectionName);
+            using (var transaction = databaseProvider.GetRequestTransaction())
+            {
+                using (var command = transaction.CreateCommand(sql))
+                {
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        return "";
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+
+                }
+            }
+        }
+        public static string ExecuteQuery(string dbConnectionName, string sql, out string result)
+        {
+            result = "";
+            var databaseProvider = DatabaseAccess.ForExternalDatabase(dbConnectionName);
+            using (var transaction = databaseProvider.GetRequestTransaction())
+            {
+                using (var command = transaction.CreateCommand(sql))
+                {
+                    try
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            //reader.Read();
+                            //resultado = reader.GetString(0);
+                            result = JsonSerializer.Serialize(reader);
+
+                        }
+                        return "";
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+                }
+            }
+        }
+
+    }
+}
